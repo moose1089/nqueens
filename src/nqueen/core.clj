67f7d680b1a6d28749cb13641e88e74eb3fb65de
mark <mark@entro-1>
2019-05-05 18:14:11 +0100

    symmetry fixed

--- src/nqueen/core.clj
+++ src/nqueen/core.clj
@@ -3,9 +3,10 @@
             [clojure.tools.trace])
   (:gen-class))
 
-;; TODO add checks for rotation / reflection
+(defn reflect-v [b]
+  (reverse b))
 
-(defn reflect [b]
+(defn reflect-h [b]
   (map reverse b))
 
 (defn rotate
@@ -15,14 +16,13 @@
                :else (rotate (rotate  b) (mod (dec n) 4)))))
 
 (defn board-eq? [a b]
-  (or (= a b)
-      (= a (reflect b))
-      (= a (rotate b 1))
-      (= a (rotate b 2))
-      (= a (rotate b 3))
-      (= a (rotate (reflect  b) 1))
-      (= a (rotate (reflect  b) 2))
-      (= a (rotate (reflect  b) 3))))
+  (let [r1 (reflect-h b)
+        r2 (reflect-v b)
+        r3 (reflect-v (reflect-h b))]
+    (or (some (partial = a) (map (partial rotate b) (range 4)))
+        (some (partial = a) (map (partial rotate r1) (range 4)))
+        (some (partial = a) (map (partial rotate r2) (range 4)))
+        (some (partial = a) (map (partial rotate r3) (range 4))))))
 
 (defn distinct-boards
   ([s]
@@ -30,16 +30,8 @@
   ([s1 [i & s2]]
    (cond
      (nil? i) s1
-     (every? #(not (board-eq? % i) s1)) (distinct-boards (cons i s1) s2)
+     (every? #(not (board-eq? % i)) s1) (distinct-boards (cons i s1) s2)
      :else (distinct-boards s1 s2))))
-
-(defn allowed? [n i board]
-  (or
-   (empty? board)
-   (and
-    (apply = " " (map #(nth % i) board))
-    (apply = " " (map #(if (< (+ i %2) n) (nth % (+ i %2)) " ") board (range (count board) 0 -1)))
-    (apply = " " (map #(if (<= 0 (- i %2)) (nth % (- i %2)) " ") board (range (count board) 0 -1))))))
 
 (defn possible-locations
   [n board]
@@ -49,30 +41,34 @@
               (let [col (concat (take i (repeat " ")) ["Q"] (take (- n i 1) (repeat " ")))]
                 col)))))
 
-  (defn nqueens* [n board]
-    (apply concat
-           (for [i (possible-locations n board)]
-             (let [new-board (concat board (list i))]
-               (if (= n (count new-board))
-                 [new-board]
-                 (nqueens* n new-board))))))
+(defn nqueens* [n board]
+  (apply concat
+         (for [i (possible-locations n board)]
+           (let [new-board (concat board (list i))]
+             (if (= n (count new-board))
+               [new-board]
+               (nqueens* n new-board))))))
 
-  (defn nqueens [n]
-    (nqueens* n []))
+(defn nqueens [n]
+  (nqueens* n []))
 
-  (defn print-solution [s]
-    (doseq        [r (range (count s))]
-      (println "|" (clojure.string/join " " (nth s r)) "|")))
+(defn print-solution [s]
+  (doseq        [r (range (count s))]
+    (println "|" (clojure.string/join " " (nth s r)) "|")))
 
-  (defn -main
-    [& args]
-    (doseq [n (range  9)]
-      (println "================================")
-      (print "n=" n)
-      (let [s (nqueens n)]
-        (println " has " (count s) "solutions," (count (distinct-boards s)) "distinct")
-        (doseq        [i (range (count s))]
-          (println "---------")
-          (println "n=" n "solution " (inc i) "of" (count s))
-          (print-solution (nth s i))
-          )))))
+(defn print-all-solutions [s]
+  (doseq        [i (range (count s))]
+    (println "---------")
+    (println "solution " (inc i) "of" (count s))
+    (print-solution (nth s i))))
+
+(defn -main
+  [& args]
+  (doseq [n (range 5 6 )]
+    (println "================================")
+    (print "n=" n)
+    (let [s (nqueens n)]
+      (println " has " (count s) "solutions," (count (distinct-boards s)) "distinct")
+      (println "distinct solutions")
+      (print-all-solutions (distinct-boards s))
+    )))
